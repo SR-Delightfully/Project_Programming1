a4a3b5fe845700a2051b97b27baf723c0a7f32b9
sr-delightfully <%email%>
2024-05-16 14:33:25 -0400

    added basic Department class, will finish validateDepartmentName method later.

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import lombok.*;
+
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Department {
+    // Field members:
+    private String departmentId;
+    private String departmentName;
+    private static int nextId;
+
+    // All arguments constructor:
+    public Department(String departmentId, String departmentName, int nextId) {
+        this.departmentId = departmentId;
+        this.departmentName = (validateDepartmentName(departmentName)) ? departmentName: null;
+        //Department.nextId = nextId;
+        this.nextId = nextId;
+    }
+
+    public static boolean validateDepartmentName(String departmentName) {
+        //TODO:
+        // check if department name contains special characters
+        // if departmentName contains special characters return false
+        // else if departmentName contains only letters or spaces return true.
+        return false;
+    }
+}
